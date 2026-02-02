230c2a2e6e4a14139a910bebcc27688f4b796f32
VGTO <vegajulian1973@gmail.com>
2026-02-01 22:38:56 -0600

    fix: stripe syntax and clean dependencies

--- src/chekout.js
+++ src/chekout.js
@@ -1,7 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
+const stripe = new Stripe(import.meta.env.VITE_STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
